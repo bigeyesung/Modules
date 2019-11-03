2d8a94b3c02aa8836c31c8bb1cbf2032552e11b2
contiliosung <sungchenhsi@gmail.com>
2019-11-03 21:56:42 +0000

    -TcpServer.cpp: bug fixed/ func add, minor change

--- CinderAsio/TcpServer.cpp
+++ CinderAsio/TcpServer.cpp
@@ -10,3 +10,8 @@ TcpServerRef TcpServer::create( asio::io_service& io )
 {
 	return TcpServerRef( new TcpServer( io ) )->shared_from_this();
 }
+
+TcpServer::TcpServer( asio::io_service& io )
+	: ServerInterface( io ), mAcceptEventHandler( nullptr ), mCancelEventHandler( nullptr )
+{
+}
