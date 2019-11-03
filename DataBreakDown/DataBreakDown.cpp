3c207050ca7cbaf51f78c6b73374773407bc60d6
contiliosung <sungchenhsi@gmail.com>
2019-11-03 22:08:58 +0000

    -DataBreakDown.cpp: bug fixed/ func add, minor change

--- DataBreakDown/DataBreakDown.cpp
+++ DataBreakDown/DataBreakDown.cpp
@@ -8,3 +8,11 @@ DataBreakDown::~DataBreakDown()
 {
 
 }
+
+void DataBreakDown::IntDataToString(const vector<int> & inData, string & outData)
+{
+	ostringstream out;
+	out << inData.size() << ' ';
+	for (int i = 0; i < inData.size(); i++)
+	{
+		out << inData[i] << ' ';
