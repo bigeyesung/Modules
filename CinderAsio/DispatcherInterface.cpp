1b5b35c0fe505e32bdee6f415c9153b223c27708
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:18:56 +0000

    -DispatcherInterface.cpp: bug fixed/ func add, minor change

--- CinderAsio/DispatcherInterface.cpp
+++ CinderAsio/DispatcherInterface.cpp
@@ -9,3 +9,8 @@ DispatcherInterface::~DispatcherInterface()
 {
 	mErrorEventHandler = nullptr;
 }
+
+void DispatcherInterface::connectErrorEventHandler( const std::function<void( std::string, size_t )>& eventHandler )
+{
+	mErrorEventHandler = eventHandler;
+}
