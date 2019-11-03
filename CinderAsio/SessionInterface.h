bf5c35b438cd9d9b695679027e2c9d290bc0e1cb
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:39:28 +0000

    -SessionInterface.h: bug fixed/ func add, minor change

--- CinderAsio/SessionInterface.h
+++ CinderAsio/SessionInterface.h
@@ -6,3 +6,8 @@
 class SessionInterface : public DispatcherInterface
 {
 public:
+
+    static std::string		bufferToString( const ci::BufferRef& buffer );
+	static ci::BufferRef	stringToBuffer( std::string& value );
+
+	~SessionInterface();
