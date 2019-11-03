895fc2b7f47e779e4d5d7e5ed09c4feeefa1c080
contiliosung <sungchenhsi@gmail.com>
2019-11-03 22:27:10 +0000

    -DirectXWindowCapture.cpp: bug fixed/ func add, minor change

--- DirectWindowCapture/DirectXWindowCapture.cpp
+++ DirectWindowCapture/DirectXWindowCapture.cpp
@@ -6,3 +6,10 @@ DirectXWindowCapture::DirectXWindowCapture()
 	m_D3dDevice = NULL;
 	m_D3dContext = NULL;
 	m_Hwnd = NULL;
+    m_DX11Device_HANDLE = NULL;
+	TextureWidth = -1;
+	TextureHigh = -1;
+	GL_Texture = 0;
+	m_MouseInfo.Ci.cbSize = sizeof(CURSORINFO);
+
+}
