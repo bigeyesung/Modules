a9378271e2c81a88818e2b44344dc23de4691b10
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:18:43 +0000

    -DispatcherInterface.cpp: bug fixed/ func add, minor change

--- CinderAsio/DispatcherInterface.cpp
+++ CinderAsio/DispatcherInterface.cpp
@@ -4,3 +4,8 @@ DispatcherInterface::DispatcherInterface( asio::io_service& io )
 	: mErrorEventHandler( nullptr ), mIoService( io ), mStrand( io )
 {
 }
+
+DispatcherInterface::~DispatcherInterface()
+{
+	mErrorEventHandler = nullptr;
+}
