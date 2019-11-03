911d2c52c42b9cdb97c199d93ec506eff61611de
contiliosung <sungchenhsi@gmail.com>
2019-11-03 22:26:55 +0000

    -DirectXWindowCapture.cpp: bug fixed/ func add, minor change

--- DirectWindowCapture/DirectXWindowCapture.cpp
+++ DirectWindowCapture/DirectXWindowCapture.cpp
@@ -1,1 +1,8 @@
 #include "DirectXWindowCapture.h"
+DirectXWindowCapture::DirectXWindowCapture()
+{
+	DX_Texture = NULL;
+	m_GdiSurface = NULL;
+	m_D3dDevice = NULL;
+	m_D3dContext = NULL;
+	m_Hwnd = NULL;
