97461615abf16731d7a4d1e3f21247ee74b86297
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:34:17 +0000

    -SessionInterface.cpp: bug fixed/ func add, minor change

--- CinderAsio/SessionInterface.cpp
+++ CinderAsio/SessionInterface.cpp
@@ -2,3 +2,12 @@
 
 using namespace ci;
 using namespace std;
+
+string SessionInterface::bufferToString( const BufferRef& buffer )
+{
+	string s( static_cast<const char*>( buffer->getData() ) );
+	if ( s.length() > buffer->getSize() ) {
+		s.resize( buffer->getSize() );
+	}
+	return s;
+}
