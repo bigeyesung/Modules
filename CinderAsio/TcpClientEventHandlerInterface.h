1545380c3bb78fea9de15188cf2245a9015749fc
contiliosung <sungchenhsi@gmail.com>
2019-11-03 21:54:48 +0000

    -TcpClientEventHandlerInterface.h: bug fixed/ func add, minor change

--- CinderAsio/TcpClientEventHandlerInterface.h
+++ CinderAsio/TcpClientEventHandlerInterface.h
@@ -2,3 +2,9 @@
 
 #include "ClientEventHandlerInterface.h"
 #include "TcpSession.h"
+
+class TcpClientEventHandlerInterface : public ClientEventHandlerInterface
+{
+public:
+	virtual void onConnect( TcpSessionRef session ) = 0;
+};
