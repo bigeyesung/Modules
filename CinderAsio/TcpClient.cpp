d1adb564c256c62a7e7832359d41472bd066108f
contiliosung <sungchenhsi@gmail.com>
2019-11-03 21:43:44 +0000

    -TcpClient.cpp: bug fixed/ func add, minor change

--- CinderAsio/TcpClient.cpp
+++ CinderAsio/TcpClient.cpp
@@ -5,3 +5,8 @@
 using namespace ci;
 using namespace std;
 using asio::ip::tcp;
+
+TcpClientRef TcpClient::create( asio::io_service& io )
+{
+	return TcpClientRef( new TcpClient( io ) )->shared_from_this();
+}
