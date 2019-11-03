004f738e15010b367cf9e9de0c55d2b276d762cf
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:11:53 +0000

    -ClientInterface.h: bug fixed/ func add, minor change

--- CinderAsio/ClientInterface.h
+++ CinderAsio/ClientInterface.h
@@ -8,3 +8,11 @@ class ClientInterface : public DispatcherInterface
 
 	virtual void		connect( const std::string& host, uint16_t port ) = 0;
 	virtual void		connect( const std::string& host, const std::string& protocol ) = 0;
+
+    template< typename T, typename Y >
+	inline void			connectResolveEventHandler( T eventHandler, Y* eventHandlerObject )
+	{
+		connectResolveEventHandler( std::bind( eventHandler, eventHandlerObject ) );
+	}
+
+	void				connectResolveEventHandler( const std::function<void ()>& eventHandler );
