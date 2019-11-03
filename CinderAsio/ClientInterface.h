c5c7db62ea522e272c24d658b9820ddf66356a31
contiliosung <sungchenhsi@gmail.com>
2019-11-03 12:11:29 +0000

    -ClientInterface.h: bug fixed/ func add, minor change

--- CinderAsio/ClientInterface.h
+++ CinderAsio/ClientInterface.h
@@ -3,3 +3,8 @@
 #include "DispatcherInterface.h"
 class ClientInterface : public DispatcherInterface
 {
+    public:
+	~ClientInterface();
+
+	virtual void		connect( const std::string& host, uint16_t port ) = 0;
+	virtual void		connect( const std::string& host, const std::string& protocol ) = 0;
