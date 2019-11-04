c85a1d8e7a79786d98d0e1e9ae3070bf7d528fe9
contiliosung <sungchenhsi@gmail.com>
2019-11-04 22:11:39 +0000

    -MeshModule.h: bug fixed/ func add, minor change

---
+++ Mesh/MeshModule.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cinder\app\AppBase.h>
+#include "cinder/gl/gl.h"
+#include "../include/ClientConst.h"
+
+using namespace ci::app;
+using namespace ci;
+
+using namespace std;
