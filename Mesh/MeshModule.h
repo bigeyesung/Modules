cc4ad0cacaba1f1472df1d9629f89fb181cc2087
contiliosung <sungchenhsi@gmail.com>
2019-11-04 22:12:01 +0000

    -MeshModule.h: bug fixed/ func add, minor change

--- Mesh/MeshModule.h
+++ Mesh/MeshModule.h
@@ -7,3 +7,12 @@ using namespace ci::app;
 using namespace ci;
 
 using namespace std;
+
+class CustomMesh
+{
+public:
+	CustomMesh();
+	~CustomMesh();
+	void CreatMesh(int Width, int Height, int ColNum, int RowNum, vector<vec2> VertexPos, vector<vec2> VertexScreenPos);
+
+	void CreatMesh(int Width, int Height, int ColNum, int RowNum, vector<joints> VertexPos);
