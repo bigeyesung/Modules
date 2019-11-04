940e9e1cb514393661a3beea0111b4f63b306a18
contiliosung <sungchenhsi@gmail.com>
2019-11-04 22:17:22 +0000

    -MeshModule.cpp: bug fixed/ func add, minor change

--- Mesh/MeshModule.cpp
+++ Mesh/MeshModule.cpp
@@ -117,3 +117,20 @@ void CustomMesh::CreatMesh(int Width, int Height, int ColNum, int RowNum, vector
 	vector<int> g_PointOne;
 	vector<int> g_PointTwo;
 	vector<int> g_PointThree;
+
+    int index = 0;
+	for (int i = 0; i < RowNum - 1; i++)
+	{
+		for (int j = 0; j < ColNum - 1; j++)
+		{
+			index = i*ColNum + j;
+
+			g_PointOne.push_back(index);
+			g_PointTwo.push_back(index + 1);
+			g_PointThree.push_back(index + ColNum + 1);
+
+			g_PointOne.push_back(index);
+			g_PointTwo.push_back(index + ColNum + 1);
+			g_PointThree.push_back(index + ColNum);
+		}
+	}
