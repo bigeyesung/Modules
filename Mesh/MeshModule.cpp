11092b52a57f16863e085f86cac1613cb953b2b7
contiliosung <sungchenhsi@gmail.com>
2019-11-04 22:19:11 +0000

    -MeshModule.cpp: bug fixed/ func add, minor change

--- Mesh/MeshModule.cpp
+++ Mesh/MeshModule.cpp
@@ -165,3 +165,8 @@ void CustomMesh::CreatMesh(int Width, int Height, int ColNum, int RowNum, vector
 		Vertex_Normal.z = 0.0;
 		Vertex_Coord.x = VertexPos.at(g_PointTwo[i]).p.x / (float)Width;
 		Vertex_Coord.y = 1 - VertexPos.at(g_PointTwo[i]).p.y / (float)Height;
+
+        VboVertices.push_back(Vertex_Pos);
+		VboNormals.push_back(Vertex_Normal);
+		VboTexCoords.push_back(Vertex_Coord);
+		VboIndices.push_back(j + 1);
