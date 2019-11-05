fb7a2644ee635d088208f4312f7f95fea64f887d
contiliosung <sungchenhsi@gmail.com>
2019-11-05 23:58:40 +0000

    -PboModule.cpp: bug fixed/ func add, minor change

--- PboModule/PboModule.cpp
+++ PboModule/PboModule.cpp
@@ -17,3 +17,9 @@ void PboProcess::creat(int width, int high, GLenum format)
 	m_iDataSize = m_Width*m_High * 3;
 	m_PboRef = ci::gl::Pbo::create(GL_PIXEL_UNPACK_BUFFER, m_iDataSize, 0, GL_STREAM_DRAW);
 }
+
+void PboProcess::update(int TextureID, Mat& Frame)
+{
+	//// bind the texture and PBO
+	glBindTexture(GL_TEXTURE_2D, TextureID);
+	glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, m_PboRef->getId());
