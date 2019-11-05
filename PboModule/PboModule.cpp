a0fc5f49a77df1c196895977a825a3b42f243f37
contiliosung <sungchenhsi@gmail.com>
2019-11-05 23:58:29 +0000

    -PboModule.cpp: bug fixed/ func add, minor change

--- PboModule/PboModule.cpp
+++ PboModule/PboModule.cpp
@@ -7,3 +7,13 @@ PboProcess::~PboProcess()
 {
 
 }
+
+void PboProcess::creat(int width, int high, GLenum format)
+{
+	m_Width = width;
+	m_High = high;
+	m_format = format;
+	//GL_BGR 
+	m_iDataSize = m_Width*m_High * 3;
+	m_PboRef = ci::gl::Pbo::create(GL_PIXEL_UNPACK_BUFFER, m_iDataSize, 0, GL_STREAM_DRAW);
+}
