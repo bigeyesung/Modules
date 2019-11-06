44df1ea27db9f955029db3a2c73db01d319e1eea
contiliosung <sungchenhsi@gmail.com>
2019-11-06 23:49:59 +0000

    -TCPbase.h: bug fixed/ func add, minor change

--- TCPclient/TCPbase.h
+++ TCPclient/TCPbase.h
@@ -10,3 +10,8 @@ using namespace boost::asio;
 typedef std::shared_ptr<ip::tcp::socket>	pTcpServerRef;
 typedef boost::asio::io_service				tIOService;
 typedef std::shared_ptr<tIOService>			tIOServiceRef;
+class TCPbase
+{
+public:
+	TCPbase();
+	~TCPbase();
