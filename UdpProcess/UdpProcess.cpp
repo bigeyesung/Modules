d43bf987c84a795209cadbf03769319e0099a763
contiliosung <sungchenhsi@gmail.com>
2019-11-07 21:02:05 +0000

    -UdpProcess.cpp: bug fixed/ func add, minor change

--- UdpProcess/UdpProcess.cpp
+++ UdpProcess/UdpProcess.cpp
@@ -8,3 +8,12 @@ UdpProcess::UdpProcess()
 UdpProcess::~UdpProcess()
 {
 }
+
+void UdpProcess::init(bool broadcast, int32_t iPort, std::string strHost)
+{
+	// Initialize a client by passing a boost::asio::io_service to it.
+	// ci::App already has one that it polls on update, so we'll use that.
+	// You can use your own io_service, but you will have to manage it 
+	// manually (i.e., call poll(), poll_one(), run(), etc).
+	//m_ioService = shared_ptr<asio::io_service>(new asio::io_service());
+	mClient = UdpClient::create(ioserviceManager::getIoService());
