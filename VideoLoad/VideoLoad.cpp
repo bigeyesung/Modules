058b7f67478f0a635da3b5b342541669bd0878dd
contiliosung <sungchenhsi@gmail.com>
2019-11-07 21:14:09 +0000

    -VideoLoad.cpp: bug fixed/ func add, minor change

--- VideoLoad/VideoLoad.cpp
+++ VideoLoad/VideoLoad.cpp
@@ -4,3 +4,14 @@ VideoLoad::VideoLoad()
 {
 	Video_Th = NULL;
 }
+
+VideoLoad::~VideoLoad()
+{
+	EndThread();
+
+	for (int i = 0; i < m_VideoLoad.size(); i++)
+	{
+		for (int j = 0; j < m_VideoLoad[i].imageMat1.size(); j++)
+		{
+			m_VideoLoad[i].imageMat1[j].release();
+		}
