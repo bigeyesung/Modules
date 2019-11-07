68db0400ec8b4deb0db8646a4d1f3fdecd787974
contiliosung <sungchenhsi@gmail.com>
2019-11-07 20:54:22 +0000

    -UDPBroadcastClient.cpp: bug fixed/ func add, minor change

--- UDPBroadcast/UDPBroadcastClient.cpp
+++ UDPBroadcast/UDPBroadcastClient.cpp
@@ -3,3 +3,13 @@
 //#include "QDebug"  //chenhsi add
 
 using boost::asio::ip::udp;
+
+UDPBroadcastClient::UDPBroadcastClient(const int& listenport) : _socket(io_service_, udp::endpoint(udp::v4(), listenport))
+{
+	// Client binds to any address on listenport (the same port on which
+	// broadcast data is sent from server).
+	ServerIP = "None";
+	is_serverip = false;
+	ReceiveNum = 0;
+	//Listen();
+}
