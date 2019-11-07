f001163567ca7652edecf2430c1167c817bc5abf
contiliosung <sungchenhsi@gmail.com>
2019-11-07 20:58:41 +0000

    -UdpProcess.h: bug fixed/ func add, minor change

---
+++ UdpProcess/UdpProcess.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "..\Modules\CinderAsio\CinderAsio.h"
+
+#include "cinder/app/App.h"
+//#include "cinder/Font.h"
+#include "cinder/gl/gl.h"
+//#include "cinder/params/Params.h"
