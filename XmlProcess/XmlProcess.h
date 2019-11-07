427f377eaf4e9b4478bc078c66ad23b24451092e
contiliosung <sungchenhsi@gmail.com>
2019-11-07 21:22:04 +0000

    -XmlProcess.h: bug fixed/ func add, minor change

--- XmlProcess/XmlProcess.h
+++ XmlProcess/XmlProcess.h
@@ -8,3 +8,10 @@ public:
 
 	XmlProcess();
 	~XmlProcess();
+
+	void ReadXml(const string& FilePath);
+	bool GetData(s_InitData& inData);
+	bool GetData(vector<vector<joints>>& inData);
+	bool GetData(vector<s_ShaderSetData>& inData);
+	
+	void CloseXml();	
